d2e9327dd3dda255e0f3d0d94cc93f343756723e
Tobías Vega <tobiasvega1210@gmail.com>
2025-08-26 12:59:10 -0300

    feat: :sparkles: clases instanciadas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { Libro } from "./models/libro.js";
-
+import { Revista } from "./models/revista.js";
+import { Usuario } from "./models/usuario.js";
 
 const libro1 = new Libro(1, "Harry Potter y la Piedra Filosofal", "JK. Rowlling", true, 200, "Misterio");
 
 const libro2 = new Libro(2, "El resplandor", "Stephen King", false, 321);
+
+const revista1 = new Revista(1, "El mundo", "Juan Pérez", false, "Edición 3");
+
+const usuario1 = new Usuario(1, "Carlos");
+
+usuario1.prestar(libro1);
+usuario1.prestar(libro2);
+usuario1.prestar(revista1);
+
+console.log(libro1.mostrarInfo());
+console.log(revista1.mostrarInfo());
